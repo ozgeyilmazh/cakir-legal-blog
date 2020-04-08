1a29a555e118ef19a6d63d78595d01b6408d40d6
Ozge <ozgeyilmzh@gmail.com>
2020-04-09 01:00:05 +0300

    ikinci commit

---
+++ static/js/custom.js
@@ -0,0 +1,25 @@
+//add smooth scrolling
+$(document).ready(function() {
+    'use strict';
+    $('.nav-item, #scroll-to-top').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+// Add animation/ Initialize Woo
+$(document).ready(function() {
+    
+    'use strict';
+   
+    new WOW().init();
+    
+});
+
